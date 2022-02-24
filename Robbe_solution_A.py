fa1952fb0a0a5e48c4e3b20b6c3de8a351f25987
robbevandevelde <vandevelde.robbe@gmail.com>
2022-02-24 21:20:23 +0100

    omg this code for A is bonkers 😎😎😎😎😂😂😂😂🤮🤮🤮🥳

---
+++ Robbe_solution_A.py
@@ -0,0 +1,24 @@
+
+def A_hyperscalingrussianturbosolution(name):
+    file = open(name, "w")
+
+    file.write("3")
+    file.write("\n")
+    file.write("WebServer")
+    file.write("\n")
+    file.write("Bob Anna")
+    file.write("\n")
+    file.write("Logging")
+    file.write("\n")
+    file.write("Anna")
+    file.write("\n")
+    file.write("WebChat")
+    file.write("\n")
+    file.write("Maria Bob")
+    file.write("\n")
+
+    file.close()
+
+
+if __name__ == '__main__':
+    A_hyperscalingrussianturbosolution("a_an_example.in.txt.out")
